4f4af0b253e1d000afd0fc17030916888b8269c8
grant <grantnpjack@gmail.com>
2020-04-28 17:22:40 +0800

    feat(random): new random fxn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 var randomitem = require('random-item');
+var uniquerandomarray = require('unique-random-array');
 var starwarsnames = require('./starwars-names.json');
+var getrandomitem = uniquerandomarray(starwarsnames);
 
 module.exports = {
     all: starwarsnames,
-    random: randomitem(starwarsnames)
+    random: random
 };
+
+function random(number) {
+    if (number == undefined) {
+        return getrandomitem();
+    } else {
+        var randomitem = [];
+        for (var i = 0; i < number; i = i + 1) {
+            randomitem.push(getrandomitem());
+        }
+        return randomitem;
+    }
+}
--- src/index.test.js
+++ src/index.test.js
@@ -15,5 +15,13 @@ describe('starwarsnamesgrantpar', function () {
         it('should contain Luke Skywalker', function () {
             expect(starwars.all).to.include('Luke Skywalker');
         });
+
+        it('should return a array of random items if passed a number', function () {
+            var randomitems = starwars.random(3);
+            expect(randomitems).to.have.length(3);
+            randomitems.forEach(function (item) {
+                expect(starwars.all).to.include(item);
+            });
+        });
     });
 });
